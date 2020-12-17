73d775882c99ceced2b5639a8d54c7d9375cd9cf
yieldly1 <75343938+yieldly1@users.noreply.github.com>
2020-12-18 08:32:03 +1100

    Update 2-donate.sh

--- devrel/crowdfunding/2-donate.sh
+++ devrel/crowdfunding/2-donate.sh
@@ -14,20 +14,20 @@ WALLET=$1
 # Directory of this bash program
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-gcmd="../../goal -d ../test/Node"
+gcmd="goal"
 
-ACCOUNT=$(${gcmd} account list|awk '{ print $3 }'|head -n 1)
+ACCOUNT="MT2G6WAQTSWVZFFTBUVYDOJ2OMW5KIS4IHAKRXDDCTA62IX56K3TSACJF4"
 
-${gcmd} app optin  --app-id 1 --from $ACCOUNT 
+${gcmd} app optin  --app-id 13299585 --from $ACCOUNT 
 
-${gcmd} app call --app-id 1 --app-arg "str:donate" --from=$ACCOUNT  --out=unsginedtransaction1.tx
-${gcmd} clerk send --from=$ACCOUNT --to="F4HJHVIPILZN3BISEVKXL4NSASZB4LRB25H4WCSEENSPCJ5DYW6CKUVZOA" --amount=500000 --out=unsginedtransaction2.tx
+${gcmd} app call --app-id 13299585 --app-arg "str:donate" --from=$ACCOUNT  --out=unsginedtransaction1.tx
+${gcmd} clerk send --from=$ACCOUNT --to="MT2G6WAQTSWVZFFTBUVYDOJ2OMW5KIS4IHAKRXDDCTA62IX56K3TSACJF4" --amount=500000 --out=unsginedtransaction2.tx
 cat unsginedtransaction1.tx unsginedtransaction2.tx > combinedtransactions.tx
 ${gcmd} clerk group -i combinedtransactions.tx -o groupedtransactions.tx 
 ${gcmd} clerk sign -i groupedtransactions.tx -o signout.tx
 ${gcmd} clerk rawsend -f signout.tx
-${gcmd} app read --app-id 1 --guess-format --global --from $ACCOUNT
-${gcmd} app read --app-id 1 --guess-format --local --from $ACCOUNT
+${gcmd} app read --app-id 13299585 --guess-format --global --from $ACCOUNT
+${gcmd} app read --app-id 13299585 --guess-format --local --from $ACCOUNT
 
 rm *.tx
 
